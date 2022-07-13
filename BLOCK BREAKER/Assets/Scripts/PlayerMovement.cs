07502e22c1f18c1eb15ec82f390545d1275fb168
alireza esmaeili <alirezaesmaeili420@gmail.com>
2022-07-13 20:23:50 +0430

    Revert "final design"
    This reverts commit 858fbd3716ed6494e596a1bbf5548ce0bfcbacab.

---
+++ BLOCK BREAKER/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{   
+    
+    public float PlayerSpeed = 20 ; 
+    public Rigidbody rb;
+    void Start()
+    {
+        
+    }
+    void FixedUpdate() 
+    {
+        if(Input.GetKey("a"))
+        {
+          transform.Translate(new Vector3(-PlayerSpeed*Time.deltaTime,0,0));
+        }
+        if(Input.GetKey("d"))
+        {
+           transform.Translate(new Vector3(PlayerSpeed*Time.deltaTime,0,0));
+        }
+    }
+}
